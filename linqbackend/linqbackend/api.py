cbb43e08e2013863a0233e6d9ec215be0d715a48
Samuel Holzer <sholzer20@my.whitworth.edu>
2020-03-05 22:11:25 -0800

    Created Windows virtual environment
    Started to establish the CRUD API

---
+++ linqbackend/linqbackend/api.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from instagram import api_views as myapp_views
+
+router = routers.DefaultRouter()
+router.register(r'friends', myapp_views.FriendViewset)
+router.register(r'belongings', myapp_views.BelongingViewset)
+router.register(r'borrowings', myapp_views.BorrowedViewset)
