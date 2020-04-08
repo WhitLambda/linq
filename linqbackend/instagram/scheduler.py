33c430fec90c91859ff6d7c4305664fa007d43e1
Lauren Barry <laurenbarry00@gmail.com>
2020-04-07 21:07:31 -0700

    Added code for scheduled tasks

---
+++ linqbackend/instagram/scheduler.py
@@ -0,0 +1,9 @@
+from background_task import background
+
+# repeat task hourly
+@background(repeat=Task.HOURLY)
+def request_instagram():
+	# for (user : linqUsers):
+	#	GET last ~20 Instagram posts
+	#	for (comment : comments):
+	#		POST comment to Linq API (overwrite)
--- linqbackend/linqbackend/settings.py
+++ linqbackend/linqbackend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'instagram'
+    'instagram',
+    'background_task'
 ]
 
 MIDDLEWARE = [
