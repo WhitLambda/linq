ad2d3d396a702ad5b7bf1448d2adcd8c18cbff22
Samuel Holzer <sholzer20@my.whitworth.edu>
2020-04-16 13:59:43 -0700

    Cleaned up repository
    Removed virtual environments from the repository. Refer to documentation for setting up your local virtual environment.

--- WinVenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\samho\downloads\software engineering linq\linq\winvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
