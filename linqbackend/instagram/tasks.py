f813280b9f61d9f9f73996716a3ae0ef3849aef9
amonrs <amonsthapit@live.com>
2020-04-23 16:20:59 -0700

    added sample tasks to ig/yt apps

--- linqbackend/instagram/tasks.py
+++ linqbackend/instagram/tasks.py
@@ -11,17 +11,7 @@ from .models import Comments
 
 @periodic_task(run_every=(crontab(minute='*/1')), name='write_to_db_insta', ignore_result=True)
 def write_to_db_insta():
-    lettersdigits = string.digits + string.ascii_letters
-    u = 'cb_lu_' + ''.join(random.choice(lettersdigits) for i in range(5))
-    ms = ''.join(random.choice(lettersdigits) for i in range(15))
-
-
-    igc_cel_b = Comments.objects.create(
-        username = u,
-        message = ms,
-    )
-
-    print('wrote object {} to the ig_cmts database :) \n'.format(igc_cel_b))
+    print("instagram task done :)\n")
 
 
 # just an example of running a periodic task
--- linqbackend/youtube/tasks.py
+++ linqbackend/youtube/tasks.py
@@ -2,7 +2,8 @@ from celery.task import periodic_task
 from celery.schedules import crontab
 
 
-@periodic_task(run_every=(crontab(minute='*/30')), name='youtube_task_1', ignore_result=True)
+@periodic_task(run_every=(crontab(minute='*/2')), name='youtube_task_1', ignore_result=True)
 def youtube_task_1():
     # run every 30 minutes
     # do something here
+    print("youtube task done :)\n")
