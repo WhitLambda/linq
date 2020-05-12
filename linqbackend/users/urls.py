51f48408838fdbb89c1af550ed73764f4fad8402
amonrs <amonsthapit@live.com>
2020-05-11 21:34:44 -0700

    added test views for react frontend to connect to

--- linqbackend/users/urls.py
+++ linqbackend/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import user_view, login_view, signup_view
+from .views import user_view, login_view, signup_view, getkeywords_view, getcomments_view
 
 urlpatterns = [
     path('', user_view.as_view(), name='user_view'),
     path('signup/', signup_view.as_view(), name='signup'),
     path('login/', login_view.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('getkeywords/', getkeywords_view.as_view(), name='getkeywords'),
+    path('getcomments/', getcomments_view.as_view(), name='getcomments')
 ]
--- linqbackend/users/views.py
+++ linqbackend/users/views.py
@@ -69,3 +69,245 @@ class login_view(APIView):
             return Response({
                 "error": form.errors
             })
+
+
+
+# current username
+
+
+
+
+
+
+
+class getkeywords_view(APIView):
+    def get(self, request):
+        # get current user and the keywords for that user in the database
+
+        return Response(
+            {
+                "username": "some_user",
+                "keywords": [
+                    {
+                        "keyword": "stream schedule",
+                        "responses": [
+                            "I'm streaming MWF at 10am",
+                            "I stream 3 times a week"
+                        ],
+                        "autoreply": "true"
+                    },
+                    {
+                        "keyword": "nice video",
+                        "responses": [
+                            "Thanks!",
+                            "Thank you!"
+                        ],
+                        "autoreply": "true"
+                    },
+                    {
+                        "keyword": "how are you",
+                        "responses": [
+                            "Doing well thanks!",
+                            "I'm great, how are you?"
+                        ],
+                        "autoreply": "true"
+                    }
+                ]
+            }
+        )
+
+    def post(self, request):
+        pass
+
+# view for user data and keywords
+# and also put request for updating user's keywords
+## example:
+    # {
+    #     "username": "some_user",
+    #     "keywords": [
+    #         {
+    #             "keyword": "stream schedule",
+    #             "responses": [
+    #                 "I'm streaming MWF at 10am",
+    #                 "I stream 3 times a week"
+    #             ],
+    #             "autoreply": true
+    #         },
+    #         {
+    #             "keyword": "nice video",
+    #             "responses": [
+    #                 "Thanks!",
+    #                 "Thank you!"
+    #             ],
+    #             "autoreply": true
+    #         },
+    #         {
+    #             "keyword": "how are you",
+    #             "responses": [
+    #                 "Doing well thanks!",
+    #                 "I'm great, how are you?"
+    #             ],
+    #             "autoreply": true
+    #         }
+    #     ]
+    # }
+
+
+
+
+
+
+class getcomments_view(APIView):
+    def get(self, request):
+        # get current user and return all their comments
+        print('getcomments get request made ...')
+
+        return Response(
+            {
+                "comments": [
+                    {
+                        "username": "xXdr4g0n_sl4y3rXx",
+                        "timestamp": "2020-01-22 22:19:46",
+                        "commentId": "1234567890",
+                        "commentText": "hey what is your streaming schedule? aaaaaaaaaaaaaaa",
+                        "keywords": [
+                            "streaming schedule"
+                        ],
+                        "platform": "twitter",
+                        "mediaId": "0987654321"
+                    },
+                    {
+                        "username": "fan_of_urs123",
+                        "timestamp": "2020-01-11 11:21:45",
+                        "commentId": "1234567890",
+                        "commentText": "this was a great video! aaaaaaaaaaaa",
+                        "keywords": [
+                            "great video"
+                        ],
+                        "platform": "youtube",
+                        "mediaId": "0987654321"
+                    },
+                    {
+                        "username": "catzilla",
+                        "timestamp": "2020-02-08 19:06:31",
+                        "commentId": "1234567890",
+                        "commentText": "Where did you get that food? aaaaaaaaaaaaaaaaa",
+                        "keywords": [],
+                        "platform": "twitch",
+                        "mediaId": "0987654321"
+                    },
+                ]
+            }
+        )
+
+    def post(self, request):
+        pass
+
+# example:
+    # {
+    # 	"comments": [
+    # 		{
+    # 			"username": "xXdr4g0n_sl4y3rXx",
+    # 			"timestamp": "2020-01-22 22:19:46",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "hey what is your streaming schedule?",
+    # 			"keywords": [
+    # 				"streaming schedule"
+    # 			],
+    # 			"platform": "twitter",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "fan_of_urs123",
+    # 			"timestamp": "2020-01-11 11:21:45",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "this was a great video!",
+    # 			"keywords": [
+    # 				"great video"
+    # 			],
+    # 			"platform": "youtube",
+    # 			"mediaId": "0987654321"
+    # 		},
+    #     {
+    # 			"username": "CopperCorgi",
+    # 			"timestamp": "2020-04-03 23:58:20",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "Your art is lame >:(",
+    # 			"keywords": [],
+    # 			"platform": "reddit",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "catzilla",
+    # 			"timestamp": "2020-02-08 19:06:31",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "Where did you get that food?",
+    # 			"keywords": [],
+    # 			"platform": "twitch",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "fan_of_urs123",
+    # 			"timestamp": "2020-02-03 21:32:04",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "like if you're part of the notification squad!!!",
+    # 			"keywords": [
+    # 				"notification squad"
+    # 			],
+    # 			"platform": "youtube",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "orangeyouglad",
+    # 			"timestamp": "2020-02-27 00:23:37",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "I like your cat",
+    # 			"keywords": [],
+    # 			"platform": "twitch",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "xXdr4g0n_sl4y3rXx",
+    # 			"timestamp": "2020-03-22 16:01:23",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "Where do we donate? This is a great video!",
+    # 			"keywords": [
+    # 				"donate",
+    #         "great video"
+    # 			],
+    # 			"platform": "youtube",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "some_kinda_spam_guy",
+    # 			"timestamp": "2020-02-22 13:15:52",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "sub for sub?",
+    # 			"keywords": [
+    # 				"sub for sub"
+    # 			],
+    # 			"platform": "youtube",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "double_triple_quad234",
+    # 			"timestamp": "2020-01-23 17:24:33",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "Love this content! Can't wait for more!",
+    # 			"keywords": [],
+    # 			"platform": "twitter",
+    # 			"mediaId": "0987654321"
+    # 		},
+    # 		{
+    # 			"username": "xXdr4g0n_sl4y3rXx",
+    # 			"timestamp": "2020-02-03 01:15:30",
+    # 			"commentId": "1234567890",
+    # 			"commentText": "omg I love you",
+    # 			"keywords": [
+    # 				"I love you"
+    # 			],
+    # 			"platform": "reddit",
+    # 			"mediaId": "0987654321"
+    # 		}
+    # 	]
+    # }
